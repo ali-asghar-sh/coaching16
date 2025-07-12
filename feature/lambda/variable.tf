b84638016f2bf2ab789d26a45b3f8061385924aa
Zhi Hong <103106663+zacharykwek15@users.noreply.github.com>
2025-07-12 12:10:08 +0800

    Delete feature/lambda directory

--- feature/lambda/variable.tf
+++
@@ -1,7 +0,0 @@
-
-##This is added to update the Dynamodb name so we can assign variable instead of hardcoding the name 
-
-variable "db_name" {
-  description = "Name of the DynamoDB table used by the Lambda functions"
-  type        = string
-}
