fe4294fac67294173674b047f1a000747c768c72
ali-asghar-sh <ali.asghar.sh@live.com>
2025-07-12 12:20:03 +0800

    Waf after fmt

--- WAF.tf
+++ WAF.tf
@@ -21,7 +21,7 @@ resource "aws_wafv2_web_acl_logging_configuration" "api_gw_waf_logging" {
   }
 }
 resource "aws_cloudwatch_log_group" "waf_logs" {
-  name              = "aws-waf-logs-api-group2-gw"  # Fixed: Added valid name
+  name              = "aws-waf-logs-api-group2-gw" # Fixed: Added valid name
   retention_in_days = 14
   tags = {
     Environment = "prod"
