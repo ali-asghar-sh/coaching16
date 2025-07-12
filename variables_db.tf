78c2e537ba1e7a1667e7875b74fd170dc068db6f
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-12 12:36:23 +0800

    Adjusted feature branch

--- variables_db.tf
+++
@@ -1,7 +0,0 @@
-# variables_db.tf
-
-variable "dynamodb_table_name" {
-  description = "DynamoDB table for the URL shortener."
-  type        = string
-  default     = "group2-url-shortener-table"
-}
