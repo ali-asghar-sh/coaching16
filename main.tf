f36a97da1ec6e175ecc48304b407404a923a5e32
zacharykwek15 <zhi_hong_sn@yahoo.com.sg>
2025-07-12 12:50:22 +0800

    add IAM role

--- main.tf
+++ main.tf
@@ -30,6 +30,11 @@ resource "aws_lambda_function" "retrieve_url_lambda" {
   runtime          = "python3.12"
   source_code_hash = filebase64sha256("retrieve_lambda.zip")
 
+
+  tracing_config {
+    mode = "Active"
+  }
+
   environment {
     variables = {
       REGION_AWS = "ap-southeast-1"
@@ -47,6 +52,10 @@ resource "aws_lambda_function" "create_url_lambda" {
   runtime          = "python3.12"
   source_code_hash = filebase64sha256("create_lambda.zip")
 
+  tracing_config {
+    mode = "Active"
+  }
+
   environment {
     variables = {
       APP_URL    = "https://short.ly"
@@ -56,4 +65,109 @@ resource "aws_lambda_function" "create_url_lambda" {
       DB_NAME    = var.db_name       #Need to update Dynamodb Table name in variable.tf
     }
   }
+}
+
+#Creating the IAM role for create lambda function
+resource "aws_iam_role" "create_lambda_exec" {
+  name = "create_lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "create_lambda_policy" {
+  name = "create_lambda_policy"
+  role = aws_iam_role.create_lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:PutItem",
+          "dynamodb:GetItem"
+        ],
+        Resource = "*"  # Replace with specific DynamoDB ARN for tighter security
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+
+#Creating IAM role for retrieve URL lambda
+resource "aws_iam_role" "retrieve_lambda_exec" {
+  name = "retrieve_lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "retrieve_lambda_policy" {
+  name = "retrieve_lambda_policy"
+  role = aws_iam_role.retrieve_lambda_exec.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:UpdateItem"
+        ],
+        Resource = "*"  # Replace with specific DynamoDB ARN
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
 }
